09fcbc47ecae72ccb08b2e9e956d02dddaba3bfe
SamyCode <sambea2103@gmail.com>
2023-12-06 00:01:04 -0500

    Create navbar

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,9 +1,11 @@
+import NavbarRoutes from '@/components/navbar-routes'
 import { MobileSidebar } from './mobile-sidebar'
 
 export function Navbar () {
   return (
     <nav className="p-4 border-b h-full flex items-center shadow-sm bg-white">
       <MobileSidebar />
+      <NavbarRoutes />
     </nav>
   )
 }
---
+++ components/navbar-routes.tsx
@@ -0,0 +1,9 @@
+import { UserButton } from '@clerk/nextjs'
+
+export default function NavbarRoutes () {
+  return (
+    <div className="flex gap-x-2 ml-auto">
+      <UserButton />
+    </div>
+  )
+}
